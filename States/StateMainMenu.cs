a6b02eb4c5fc94c136e0907133ba78f336d12b88
Mat <mathewcausby@hotmail.com>
2018-05-07 19:17:21 +0800

    Main Menu Polish

--- States/StateMainMenu.cs
+++ States/StateMainMenu.cs
@@ -17,8 +17,18 @@ namespace Assignment2.States
         private StateManager m_stateManager;
         private SpriteBatch m_spriteBatch;
         private ContentManager m_content;
-        private Texture2D m_titleScreen;
-        private SpriteFont m_font;
+        private Texture2D m_blackboard;
+        private Texture2D m_titleText;
+        private Texture2D m_playGameText;
+        private Texture2D m_optionsText;
+        private Texture2D m_exitText;
+
+        // Texture Positioning
+        private Rectangle m_titleTextRectangle;
+        private Rectangle m_playGameTextRectangle;
+        private Rectangle m_optionsTextRectangle;
+        private Rectangle m_exitTextRectangle;
+
         private KeyboardState m_oldKeyState;
         private int m_option = 0;
 
@@ -31,8 +41,21 @@ namespace Assignment2.States
 
         public void Init()
         {
-            m_titleScreen = m_content.Load<Texture2D>("Title");
-            m_font = m_content.Load<SpriteFont>("Selector");
+            m_blackboard = m_content.Load<Texture2D>("Blackboard");
+            m_titleText = m_content.Load<Texture2D>("TitleText");
+            m_playGameText = m_content.Load<Texture2D>("PlayGameText");
+            m_optionsText = m_content.Load<Texture2D>("OptionsText");
+            m_exitText = m_content.Load<Texture2D>("ExitText");
+
+            int expectedWidth = m_blackboard.Width;
+            int expectedHeight = m_blackboard.Height;
+            float adjustedWidthFactor = m_spriteBatch.GraphicsDevice.DisplayMode.Width / (float)expectedWidth;
+            float adjustedHeightFactor = m_spriteBatch.GraphicsDevice.DisplayMode.Height / (float)expectedHeight;
+            
+            m_titleTextRectangle = new Rectangle((int)(86 * adjustedWidthFactor), (int)(49 * adjustedHeightFactor), (int)(m_titleText.Width * adjustedWidthFactor), (int)(m_titleText.Height * adjustedHeightFactor));
+            m_playGameTextRectangle = new Rectangle((int)(657 * adjustedWidthFactor), (int)(201 * adjustedHeightFactor), (int)(m_playGameText.Width * adjustedWidthFactor), (int)(m_playGameText.Height * adjustedHeightFactor));
+            m_optionsTextRectangle = new Rectangle((int)(657 * adjustedWidthFactor), (int)(358 * adjustedHeightFactor), (int)(m_optionsText.Width * adjustedWidthFactor), (int)(m_optionsText.Height * adjustedHeightFactor));
+            m_exitTextRectangle = new Rectangle((int)(657 * adjustedWidthFactor), (int)(509 * adjustedHeightFactor), (int)(m_exitText.Width * adjustedWidthFactor), (int)(m_exitText.Height * adjustedHeightFactor));
         }
         
         public void Update(GameTime time)
@@ -64,8 +87,11 @@ namespace Assignment2.States
         public void Draw(GameTime time)
         {
             m_spriteBatch.Begin();
-            m_spriteBatch.Draw(m_titleScreen, new Rectangle(0, 0, m_spriteBatch.GraphicsDevice.DisplayMode.Width, m_spriteBatch.GraphicsDevice.DisplayMode.Height), Color.White);
-            m_spriteBatch.DrawString(m_font, ">", new Vector2(m_spriteBatch.GraphicsDevice.DisplayMode.Width / 1.6f, 350 + (m_option * 260)), Color.White);
+            m_spriteBatch.Draw(m_blackboard, new Rectangle(0, 0, m_spriteBatch.GraphicsDevice.DisplayMode.Width, m_spriteBatch.GraphicsDevice.DisplayMode.Height), Color.White);
+            m_spriteBatch.Draw(m_titleText, m_titleTextRectangle, Color.White);
+            m_spriteBatch.Draw(m_playGameText, m_playGameTextRectangle, (m_option == 0) ? Color.Tan : Color.White);
+            m_spriteBatch.Draw(m_optionsText, m_optionsTextRectangle, (m_option == 1) ? Color.Tan : Color.White);
+            m_spriteBatch.Draw(m_exitText, m_exitTextRectangle, (m_option == 2) ? Color.Tan : Color.White);
             m_spriteBatch.End();
         }
         
